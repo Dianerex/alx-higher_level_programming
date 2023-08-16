cad49141eeb21cc58e6af4d103c6747d66954959
Dianerex <dianerex@gmail.com>
2023-08-16 16:29:01 -0700

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-// prints 3 lines: (like 1-multi_languages.js) but by
-// using an array of string and a loop
 
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < 3; i++) {
+let i = 0;
+while (i < 3) {
   console.log(langs[i]);
+  i++;
 }
