937f9e407765ec9a61987f65d563cc1c2f7970b1
Dianerex <dianerex@gmail.com>
2023-09-01 18:45:07 -0700

    0x11. Python - Network #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,9 +2,10 @@
 """0x11. Python - Network #1, task 4. What's my status? #1
 """
 
-if __name__ == "__main__":
-    from requests import get
+import requests
 
-    response = get('https://intranet.hbtn.io/status')
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(
-        type(response.text), response.text))
+response = requests.get('https://alx-intranet.hbtn.io/status')
+
+print("Body response:")
+print("\t- type:", type(response.text))
+print("\t- content:", response.text)
