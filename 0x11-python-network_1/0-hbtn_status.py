36d844e7f923d0b7c6d577dc60c03bc5aeca7b75
Dianerex <dianerex@gmail.com>
2023-09-01 18:33:15 -0700

    0x11. Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,12 +9,10 @@ try:
         html = response.read()
         content = html.decode('utf-8')
         
-        print("-" * 50)
         print("Body response:")
-        print("\t- type:", type(html))
-        print("\t- content:", content)
-        print("\t- utf8 content:", content)
-        print("-" * 50)
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html.decode('utf-8')))
+        print("\t- utf8 content: {}".format(content))
 
 except urllib.error.URLError as e:
     print("Error:", e)
