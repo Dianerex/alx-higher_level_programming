247d99b64a6f950b706785f63c728afa4430558a
Dianerex <dianerex@gmail.com>
2023-09-01 18:29:49 -0700

    0x11. Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,19 @@
 """Fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
+url = 'https://alx-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
+try:
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        content = html.decode('utf-8')
+        
+        print("-" * 50)
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- type:", type(html))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content)
+        print("-" * 50)
+
+except urllib.error.URLError as e:
+    print("Error:", e)
